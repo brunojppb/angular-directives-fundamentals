db37075aadf9201df0abc602717e66ca5fa93d98
Bruno Paulino <brunojppb@gmail.com>
2016-03-21 16:20:21 -0300

    My first directive

--- script.js
+++ script.js
@@ -8,6 +8,32 @@
     .module("app")
     .controller('mainCtrl', ['$scope', '$log', function($scope, $log){
       $log.info('Ok! Angular running!');
+
+      $scope.user = {
+        name: 'Bruno Paulino',
+        address: {
+          street: 'Rua Jose Simoes de Araujo',
+          city: 'João Pessoa',
+          planet: 'Earth'
+        },
+        friends: [
+          'Jean',
+          'Victor',
+          'Rhaisa',
+          'Lele'
+        ]
+      }
+
     }]);
+
+    // My first directive
+    angular
+      .module('app')
+      .directive('userInfoCard', function() {
+        return {
+          templateUrl: 'userInfoCard.html',
+          restrict: 'E'
+        };
+      });
 
 }());
